22f01eb102b147417308bdf3c280a98d0da8d9d6
John Farmer <jfarmer@zev.uchicago.edu>
2017-05-17 15:00:11 -0500

    Add WCG4SimMananger.hh -- oops

---
+++ include/WCG4SimManager.hh
@@ -0,0 +1,37 @@
+#ifndef WCG4_SIMMAN
+#define WCG4_SIMMAN
+#include "common.hh"
+#include <vector>
+#include "WCG4SimConfig.hh"
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "WCG4StackingAction.hh"
+//#include "TH1D.h"
+
+class WCG4SimManager {
+  private:
+
+    unsigned int runID;
+    double start, end;
+
+    std::vector<WCG4SimConfig*> configList;
+    std::vector<double> VEMList;
+    std::vector<std::vector<double> > dataList;
+    //std::vector<TH1D> histoList;
+    
+    G4UImanager* UI;
+  public:
+    WCG4SimManager();
+    void SetEndpoints(double _start, double _end) {start=_start; end=_end;};
+    void AddConfig(WCG4SimConfig* conf) {configList.push_back(conf);}
+    void GenerateStandardConfigs();
+
+    void ConfigureRunPhysics(WCG4SimConfig* theConf);
+    void EndRun();
+
+    void CalculateVEM();
+    void RunSimAllConfigs(double granularity);
+};
+
+
+#endif
